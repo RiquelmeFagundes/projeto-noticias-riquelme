e9e1e4236caf68cfab68306e077cdb67c670cda4
Riquelme Fagundes <fagundesriquelme@gmail.com>
2026-08-29 23:38:04 -0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { noticias } from '../data/noticias'
+
+export default function Home() {
+  const destaques = noticias.slice(0,5);
+
+  return (
+    <div>
+      <h1>Notícias</h1>
+      {destaques.map((noticia) => (
+        <Link key={noticia.id} to = {`/noticia/${noticia.id}`}>
+          <div>
+            <img src={noticia.foto} alt={noticia.titulo} width="200"/>
+              <h2>{noticias.titulo}</h2>
+                <p>{noticias.resumo}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
