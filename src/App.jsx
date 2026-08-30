fcfb157af52b968de64267b0ac4412ef68e6d3c9
Riquelme Fagundes <fagundesriquelme@gmail.com>
2026-08-30 00:12:57 -0300

    Not Found, App e Main criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
+import DetalhesNoticia from './pages/DetalhesNoticia'
+import NotFound from './pages/NotFound'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div></div>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element= {<Home />}></Route>
+      <Route path="noticia/:id" element={<DetalhesNoticia />}></Route>
+      </Route>
+      <Route path="*" element= {<NotFound/>}></Route>
+    </Routes>
+  );
 }
